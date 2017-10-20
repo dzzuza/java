548080c4c7f0781d6f51975f149d474d50d4992c
Zuza <dziedzic.zuzanna@gmail.com>
2017-10-20 14:33:23 +0200

    "small changes, email works"

--- src/lab_03/email/EmailBuilderMain.java
+++ src/lab_03/email/EmailBuilderMain.java
@@ -9,19 +9,6 @@ import java.util.Properties;
 
 public class EmailBuilderMain {
     public static void main(String[] argv) {
-
-/*        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Assuming you are sending email from localhost
-        String host = "localhost";
-
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);*/
-
         EmailMessage mail= null;
         try {
             mail=EmailMessage.builder()
