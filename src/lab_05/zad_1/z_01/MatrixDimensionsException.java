f34e82fcd22f6cbb683ab175f1ea98dc33da90f3
Zuza <dziedzic.zuzanna@gmail.com>
2017-10-29 22:04:56 +0100

    commit lab_05 based on matrix lab_01

---
+++ src/lab_05/zad_1/z_01/MatrixDimensionsException.java
@@ -0,0 +1,8 @@
+package lab_05.zad_1.z_01;
+import lab_01.matrix.Matrix;
+
+public class MatrixDimensionsException extends Exception {
+    public MatrixDimensionsException(String message) {
+        super(message);
+    }
+}
