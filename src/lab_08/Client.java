7a7ab7b60967594e25bf1ffacddd9a18a67fca45
Zuza <dziedzic.zuzanna@gmail.com>
2018-01-12 22:05:33 +0100

    lab6 polynominal chart works

---
+++ src/lab_08/Client.java
@@ -0,0 +1,157 @@
+package lab_08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+
+public class Client {
+    private Socket echoSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String ID;
+    String state;
+/*    public Client(){
+
+    }*/
+    public void connect() {
+
+        try {
+            echoSocket = new Socket("localhost", 3000);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    echoSocket.getInputStream()));
+            System.out.println("connected");
+            /*while(true){
+              String readerInput = bufferedReader.readLine();
+              printWriter.printl(readerInput);
+            }*/
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: localhost.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                    + "the connection to: localhost.");
+            System.exit(1);
+        }
+    }
+
+    private String login(String password) throws IOException {
+        connect();
+        System.out.println("tu zaczyna sie logowanie");
+
+        String login = "szymon";
+        out.printf("LOGIN %s;%s\n", login, password);
+
+        state = in.readLine();
+
+        System.out.println("zalogowany: "+state);
+
+
+
+        if(state.equals("true")){
+            ID = in.readLine();
+            System.out.println("id: "+ID);
+            out.close();
+            in.close();
+            echoSocket.close();
+            return ID;
+        }
+        else {
+            Integer lev = computeLevenshteinDistance(password,"lala");
+            System.out.println("odległość Levenshteina: "+lev);
+            out.close();
+            in.close();
+            echoSocket.close();
+            return null;
+        }
+
+    }
+    private void logout() throws IOException {
+
+        connect();
+
+        System.out.println("tu zaczyna sie wylogowywanie");
+        //String state;
+        out.printf("LOGOUT "+"1234567890\n");
+        state=in.readLine();
+        System.out.println("zalogowany: "+state);
+
+        out.close();
+        in.close();
+        echoSocket.close();
+    }
+
+    public String ls() throws IOException {
+        connect();
+        String output;
+        String login="szymon";
+        System.out.println("Start ls");
+        //login("lala");
+        out.printf("LS " + "1234567890\n");
+        output = in.readLine();
+
+        out.close();
+        in.close();
+        echoSocket.close();
+        System.out.println(output);
+
+        return output;
+    }
+
+    public boolean get(int number) throws IOException {
+        connect();
+        String output;
+        //System.out.println(files.length);
+        //ID=login("lala");
+        ID="1234567890";
+        out.println("GET " + ID+" "+number+"\n");
+        output=in.readLine();
+
+        out.close();
+        in.close();
+        echoSocket.close();
+        System.out.println("ooo");
+        System.out.println(output);
+        return true;
+    }
+
+//levensthein z wikipedii
+
+        private static int minimum(int a, int b, int c) {
+            return Math.min(Math.min(a, b), c);
+        }
+
+        public static int computeLevenshteinDistance(CharSequence lhs, CharSequence rhs) {
+            int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];
+
+            for (int i = 0; i <= lhs.length(); i++)
+                distance[i][0] = i;
+            for (int j = 1; j <= rhs.length(); j++)
+                distance[0][j] = j;
+
+            for (int i = 1; i <= lhs.length(); i++)
+                for (int j = 1; j <= rhs.length(); j++)
+                    distance[i][j] = minimum(
+                            distance[i - 1][j] + 1,
+                            distance[i][j - 1] + 1,
+                            distance[i - 1][j - 1] + ((lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1));
+
+            return distance[lhs.length()][rhs.length()];
+        }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        try {
+            client.login("lala");
+            client.logout();
+            client.ls();
+            client.get(2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/lab_08/Server.java
@@ -0,0 +1,165 @@
+package lab_08;
+
+import jdk.nashorn.api.tree.Tree;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Server {
+
+    static TreeMap<String, LinkedList<String>> users;
+    static TreeMap<String, String> files;
+
+    public static void main(String[] args) throws IOException {
+        String pass = "lala";
+        String id = "";
+
+        //id/user,lista plikow
+        //id/user,lista plikow
+        users = new TreeMap<>();
+        files = new TreeMap<>();
+        LinkedList list = new LinkedList();
+
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        users.put("1234567890", list);
+        files.put("aaa", "zawartość aaa");
+        files.put("bbb", "zawartość bbb");
+        files.put("ccc", "zawartość ccc");
+
+        Boolean logged = false;
+/*try {
+                serverSocket = new ServerSocket(3001);
+
+                Socket socket = serverSocket.accept();//Client socket
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message =bufferedReader.readLine();
+                System.out.println("messag received from Client"+message);
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+                printWriter.println("Server echoing back"+message);
+            } catch (IOException e) {
+                System.out.println("Could not listen on port: 3001"+e);
+                System.exit(-1);
+            }*/
+
+
+            ServerSocket serverSocket = null;
+
+            try {
+                serverSocket = new ServerSocket(3000);
+            } catch (IOException e) {
+                System.out.println("Could not listen on port: 3000");
+                System.exit(-1);
+            }
+
+        while (true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.out.println("Accept failed: 3000");
+                System.exit(-1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream()));
+            String inputLine;
+            inputLine = in.readLine();
+
+
+            if (inputLine.startsWith("LOGIN")) {
+                String passInput = inputLine.split(" ")[1].split(";")[1];
+                System.out.println(passInput);
+                if (passInput.equals(pass)) {
+                    logged = true;
+                    //ID = UUID.randomUUID().toString();
+                    //System.out.println(ID);
+                    id = "1234567890";
+                    out.println(logged);
+                    out.println(id);
+                } else {
+                    logged = false;
+                    out.println("logowanie nie powiodło się, zalogowany: " + logged);
+                }
+            }
+            if (inputLine.startsWith("LOGOUT")) {
+                System.out.println("logout");
+                String idInput = inputLine.split(" ")[1];
+                System.out.println("id" + idInput);
+                if (logged = true && idInput.equals("1234567890")) {
+                    logged = false;
+                    out.println(logged);
+                } else {
+                    out.println("error");
+                }
+
+            }
+            if (inputLine.startsWith("LS")) {
+                System.out.println(inputLine);
+                System.out.println(logged);
+                String idInput = inputLine.split(" ")[1];
+                System.out.println("id" + idInput);
+                System.out.println(logged);
+                if (idInput.equals("1234567890")) {
+                    out.println(users.get(idInput));
+                }
+            }
+            if (inputLine.startsWith("GET")) {
+                System.out.println(inputLine);
+                LinkedList<String> filenames;
+                String idInput = inputLine.split(" ")[1];
+                int number = Integer.parseInt(inputLine.split(" ")[2]);
+                if (idInput.equals("1234567890")) {
+                    filenames = (users.get(idInput));
+                        out.println(files.get(filenames.get(number)));
+                        System.out.println(files.get(filenames.get(number)));
+                    }
+                }
+            }
+        }
+    }
+
+
+/*                        Pattern pattern = Pattern.compile("(LOGIN|LOGOUT) ([^ ]+)([;]+)([^ ]+)?");
+                        Matcher matcher = pattern.matcher(inputLine);
+                        //System.out.println("1tu jestem"+matcher.group(1)+"2"+matcher.group(2)+"3"+matcher.group(3)+"4"+matcher.group(4));
+
+                        if (matcher.find()) {
+                            if (matcher.group(1).equals("LOGIN")) {
+                                if (matcher.group(4).equals(pass)) {
+                                    logged = true;
+                                    id = "1234567890";
+                                    out.println(logged);
+                                    out.println(id);
+                                } else {
+                                    logged = false;
+                                    out.println("logowanie nie powiodło się, zalogowany: " + logged);
+                                }
+                            }
+                            if (matcher.group(1).equals("LOGOUT")) {
+                                System.out.println("kto to");
+                                if(matcher.group(2).equals("1234567890")){
+                                    System.out.println("co to");
+                                    logged=false;
+                                    out.println(logged);
+                                }
+                            }
+                        }
+//                    if (matcher.group(1).equals("LS")) {}
+//                    if (matcher.group(1).equals("GET")) {}
+
+                        out.close();
+                        in.close();
+                        clientSocket.close();
+                        serverSocket.close();*/
+
+//}
